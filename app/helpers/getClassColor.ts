b928dced4e69e7333a928d6a5c7119c480ffbaf7
jfitzsimmons <jfitzcode@gmail.com>
2022-07-29 08:19:28 -0500

    step color styles, helper bug

--- app/helpers/getClassColor.ts
+++ app/helpers/getClassColor.ts
@@ -1,8 +1,10 @@
 //testjpf try this again -> import { isSingle } from './base';
+//TESTJPF move from this array stuff to object
 import { helper } from '@ember/component/helper';
 
 function getClassColor(nums: [number, number]) {
-  return nums[0] < 10 || nums[1] < 10 ? 'red' : 'gray';
+  let _nums: any = Array.from(nums);
+  return _nums![0]![0] < 10 || _nums![0]![1] < 10 ? 'red' : 'gray';
 }
 
 export default helper(getClassColor);
